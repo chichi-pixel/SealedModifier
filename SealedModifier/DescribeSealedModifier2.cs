40b66845a5424061e39a1c6499ecde0f44daa4e2
chichi-pixel <hatice.peucker@hotmail.com>
2022-08-29 23:32:53 +0200

    Sealed Modifier explained!

---
+++ SealedModifier/DescribeSealedModifier2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedModifier
+{
+    public virtual void CodingCsharp() => Console.WriteLine("Base class");
+
+
+    public class Derived : Base
+    {
+        // this Method is sealed!
+        public sealed override void CodingCsharp() => Console.WriteLine("Derived class");
+        
+    }
+    public class DerivedFromDerived : Derived
+    {
+        // no compile!already is sealed in the Derived Class!
+        public CodingCsharp()
+        {
+
+
+        }
+    }
+
+}
+      
---
+++ SealedModifier/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SealedModifier
+{
+    //it does not compile public class SealedBase! We have to use only the public class SealedBase for deriving the class!
+    public class SealedBase
+    {
+
+    }
+
+    
+    public class DerivedFromSealed : SealedBase
+    {
+        public DerivedFromSealed()
+        {
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my GitHub Account!");
+            Console.ReadKey();
+        }
+    }
+}
